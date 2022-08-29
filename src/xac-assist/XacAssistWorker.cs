a99211ad11bc40a75a81bbf594907796663dc249
Nate Yocom <nate@yocom.org>
2022-08-29 15:48:23 -0700

    Add IConfiguration, cleanup make

--- src/xac-assist/XacAssistWorker.cs
+++ src/xac-assist/XacAssistWorker.cs
@@ -3,10 +3,12 @@ namespace XacAssist {
     {
         private readonly ILogger<XacAssistWorker> _logger;
         private readonly IServiceScopeFactory _serviceScopeFactory;
+        private readonly IConfiguration _configuration;
 
-        public XacAssistWorker(ILogger<XacAssistWorker> logger, IServiceScopeFactory scopeFactory) {
+        public XacAssistWorker(ILogger<XacAssistWorker> logger, IServiceScopeFactory scopeFactory, IConfiguration configuration) {
             _logger = logger;
             _serviceScopeFactory = scopeFactory;
+            _configuration = configuration;
         }
 
         private CancellationTokenSource _internalStopToken = new CancellationTokenSource();
@@ -15,6 +17,11 @@ namespace XacAssist {
             using(IServiceScope scope = _serviceScopeFactory.CreateScope()) {                                    
                 // Endless loop until stopped...
                 _logger.LogInformation("ExecuteAsync() running"); 
+
+                string readDevice = _configuration["Devices:Input"] ?? "/dev/input/js0";
+                string writeDevice = _configuration["Devices:Output"] ?? "/dev/hidg0";
+                _logger.LogInformation($"Reading from {readDevice} and outputting to {writeDevice}");
+
                 while (!stoppingToken.IsCancellationRequested && !_internalStopToken.Token.IsCancellationRequested)
                 {                                           
                     await Task.Delay(1000, stoppingToken);
