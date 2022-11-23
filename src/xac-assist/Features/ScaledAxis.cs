10752073931e8a34f28fb37651389e7616886c32
Nate Yocom <nate@yocom.org>
2022-11-22 21:02:59 -0800

    Add scaled axis implementation

--- src/xac-assist/Features/ScaledAxis.cs
+++ src/xac-assist/Features/ScaledAxis.cs
@@ -8,8 +8,14 @@ namespace XacAssist.Features {
     public class ScaledAxis : Feature {
         public override string Name { get { return "Scaled Axis"; } }
 
+        public float ScaleAmount { get; set; } = 0.10f;
+
+
+        private ILogger<ScaledAxis> _logger;
+
         public ScaledAxis(ILoggerFactory loggerFactory, Joystick inputJoystick, SimpleJoystick outputJoystick) : 
             base(loggerFactory, inputJoystick, outputJoystick) {
+            _logger = loggerFactory.CreateLogger<ScaledAxis>();
         }
                 
         public override void Start() {
@@ -22,12 +28,25 @@ namespace XacAssist.Features {
         }
         
         public override FeatureFilterAction ButtonFilter(ref byte buttonId, ButtonEventTypes eventType, ref bool pressed, TimeSpan elapsed) {
+            // Hack for now, in the absence of a UI for configuration... button 10 means -.05, button 11 means + 0.05
+            if (eventType == ButtonEventTypes.LongPress) {
+                if (buttonId == 10) {
+                    ScaleAmount -= 0.05f;
+                    _logger.LogDebug($"Decremented scale amount to: {ScaleAmount}");
+                } else if (buttonId == 11) {
+                    ScaleAmount += 0.05f;
+                    _logger.LogDebug($"Incremented scale amount to: {ScaleAmount}");
+                }
+            }
+
             // We don't do anything with buttons
             return FeatureFilterAction.Continue;
         }
                 
         public override FeatureFilterAction AxisFilter(ref byte axisId, ref short value, TimeSpan elapsed) {
             // When we're enabled, we scale and modify the axis value, then let it flow
+            float scaledValue = ScaleAmount * ((float) value);
+            value = Convert.ToInt16(scaledValue);
             return FeatureFilterAction.Continue;
         }
     }
